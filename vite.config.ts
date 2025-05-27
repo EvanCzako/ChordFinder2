7cd78268169b8e060ed53755392b770e0e97c802
EvanCzako <evan.czako@gmail.com>
2025-05-27 12:46:22 -0400

    Update vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-    plugins: [solidPlugin()],
-    server: {
-        port: 3000,
-    },
-    build: {
-        target: "esnext",
-    },
+  plugins: [solidPlugin()],
+  base: '/ChordFinder2/', // <-- set this to your GitHub repo name
 });
