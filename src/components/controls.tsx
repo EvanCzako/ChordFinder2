9fe9dd4002c2289504764934448f8e4253ca10e6
EvanCzako <evan.czako@gmail.com>
2025-06-17 15:47:49 -0400

    Remove clear button temporarily for testing

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -171,13 +171,13 @@ const Controls: Component<{}> = (props: {}) => {
 				/>
 			</div>
 
-			<button style={{"font-size": `${store.dispSize}px`}} class={styles.controlsButton}
+			{/* <button style={{"font-size": `${store.dispSize}px`}} class={styles.controlsButton}
                 on:click={() => {
                     clearAllNotes();
                 }}
             >
                 Clear
-            </button>
+            </button> */}
 
         </div>
     );
